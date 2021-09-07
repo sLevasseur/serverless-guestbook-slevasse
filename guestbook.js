762a7adcec11eda9d3fff99603345560484a1424
SLevasseur <37228391+sLevasseur@users.noreply.github.com>
2021-09-07 12:15:14 +0200

    modify api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '/guestbook';
+const apiUrl = 'https://37067f83.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
